1e970c9169b6117f197b362c4974d26e1a5d065f
Md. Rony <rony15-10527@diu.edu.bd>
2022-09-04 21:58:05 +0600

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  daisyui: {
+    themes: ["emerald"],
+  },
+  plugins: [require("daisyui")],
+};
